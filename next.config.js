5d0c4ec7a3650521747e2ee5979710ec22eeff21
Mehmet Cinar <mcinarpro@gmail.com>
2022-04-05 22:52:15 +0200

    try to save page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NOTION_API_KEY: process.env.NOTION_API_KEY,
+    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/books/new.tsx
+++ pages/books/new.tsx
@@ -1,10 +1,28 @@
 import type { NextPage } from "next";
 import { useForm } from "react-hook-form";
+import { notion } from "../../config/notion";
 
 const NewBookPage: NextPage = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const onSubmit = async (data: any) => {
+    console.log(process.env.NOTION_DATABASE_ID!);
+    const response = await notion.pages.create({
+      parent: {
+        database_id: process.env.NOTION_DATABASE_ID!,
+      },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: "Tuscan Kale",
+              },
+            },
+          ],
+        }
+      },
+    });
+    console.log(response)
   };
 
   return (
@@ -20,7 +38,7 @@ const NewBookPage: NextPage = () => {
             required: "Please enter an author.",
           })}
         />
-        <input type="submit" value="Add book"/>
+        <input type="submit" value="Add book" />
       </form>
     </>
   );
